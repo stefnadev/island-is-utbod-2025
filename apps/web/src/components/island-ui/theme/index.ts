659b6c38007737e775bbb70bc2033c868995ff4e
Ármann Pétur Ævarsson <armann@stefna.is>
2025-05-05 18:21:47 +0000

    Add island-ui components

---
+++ apps/web/src/components/island-ui/theme/index.ts
@@ -0,0 +1,4 @@
+import * as HelperStylesExports from './helperStyles.css';
+export const helperStyles = { ...HelperStylesExports };
+export * from './colors/colors';
+export * from './theme';
--- apps/web/src/screens/Home/Home.tsx
+++ apps/web/src/screens/Home/Home.tsx
@@ -1,5 +1,11 @@
 import * as styles from './Home.css';
+import { Button } from '../../components/island-ui/core/src';
 
 export const Home = () => {
-  return <div className={styles.container}>Halló heimur</div>;
+  return (
+    <div className={styles.container}>
+      <Button>Halló heimur</Button>
+      Halló heimur
+    </div>
+  );
 };
