bdeffcc88f02eb72df4a61670bc3787b39f990ff
runarvestmann <runar@stefna.is>
2025-05-06 11:18:09 +0000

    Read from env variable

--- apps/web/src/utils/apolloClient.ts
+++ apps/web/src/utils/apolloClient.ts
@@ -6,7 +6,8 @@ export function createApolloClient() {
   return new ApolloClient({
     ssrMode: true,
     link: new HttpLink({
-      uri: 'http://localhost:3000/graphql', // TODO: read this from environment variable
+      uri:
+        process.env.GATEWAY_API_GRAPHQL_URL || 'http://localhost:3000/graphql',
       fetch,
     }),
     cache: new InMemoryCache(),
