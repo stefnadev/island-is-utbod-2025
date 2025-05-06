93380b96eb91b2fcece726e58af2427acd78556a
Rúnar Vestmann <runar@stefna.is>
2025-05-06 20:02:16 +0000

    feat(web): Add font and global styles (#15)

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,3 +1,11 @@
 export enum CookieKeys {
   ACCESS_TOKEN = 'access_token',
 }
+
+export const PRELOADED_FONTS = [
+  '/fonts/ibm-plex-sans-v7-latin-300.woff2',
+  '/fonts/ibm-plex-sans-v7-latin-regular.woff2',
+  '/fonts/ibm-plex-sans-v7-latin-italic.woff2',
+  '/fonts/ibm-plex-sans-v7-latin-500.woff2',
+  '/fonts/ibm-plex-sans-v7-latin-600.woff2',
+];
---
+++ apps/web/src/styles/globals.css.ts
@@ -0,0 +1,89 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html', {
+  boxSizing: 'border-box',
+  fontSize: 18,
+});
+
+globalStyle('body', {
+  overflowX: 'hidden',
+});
+
+globalStyle('*, *:before, *:after', {
+  boxSizing: 'inherit',
+});
+
+globalStyle('body, h1, h2, h3, h4, h5, h6, p, ol, ul, blockquote', {
+  margin: 0,
+  padding: 0,
+  fontWeight: 'normal',
+  fontFamily: 'IBM Plex Sans, San Francisco, Segoe UI, sans-serif',
+  color: '#00003c',
+  scrollMarginTop: 64,
+});
+
+globalStyle('strong', {
+  fontWeight: 600,
+});
+
+// NOTE: DO NOT reset `ol[type]` <-- !!!!!
+// Since CSS attribute selectors for most standard HTML attributes
+// are case-insensitive it becomes impossible to reapply the
+// default styles for `<ol type="A">` and `<ol type="a">`
+// ...and `<ol type="I">` and `<ol type="i">`
+// This is important because the ol[type] attribute is semantic,
+// not presentational, as text content may use wording such as
+// "see item C below" or "according to item viii above"
+globalStyle('ol:not([type]), ul', {
+  listStyle: 'none',
+});
+
+globalStyle('a', {
+  textDecoration: 'none',
+  color: 'inherit',
+});
+
+globalStyle('a:hover', {
+  textDecoration: 'underline',
+});
+
+globalStyle('img', {
+  maxWidth: '100%',
+  height: 'auto',
+});
+
+globalStyle('button, input, optgroup, select, textarea', {
+  margin: 0,
+  fontFamily: 'inherit',
+  fontSize: 'inherit',
+  fontWeight: 'inherit',
+  color: 'inherit',
+  lineHeight: 'inherit',
+});
+
+globalStyle('button', {
+  display: 'inline-block',
+  border: 'none',
+  background: 'transparent',
+  padding: 0,
+  margin: 0,
+  textDecoration: 'none',
+  fontSize: 'inherit',
+  lineHeight: 1,
+  cursor: 'pointer',
+  textAlign: 'center',
+  appearance: 'none',
+});
+
+globalStyle('.visually-hidden', {
+  position: 'absolute',
+  height: '1px',
+  width: '1px',
+  overflow: 'hidden',
+  clip: 'rect(1px, 1px, 1px, 1px)',
+  whiteSpace: 'nowrap',
+});
+
+globalStyle('.js-focus-visible :focus:not(.focus-visible)', {
+  outline: 'none',
+});
