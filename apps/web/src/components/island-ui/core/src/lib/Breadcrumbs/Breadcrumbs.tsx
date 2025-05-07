b7ef7a107e9830ad4548d44dc53387bf5e63cdb5
Ármann Pétur Ævarsson <armann@stefna.is>
2025-05-07 17:01:51 +0000

    feat(web): Add sidebar content and breadcrumbs (#29)

---
+++ apps/web/src/components/island-ui/core/src/lib/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,99 @@
+import React, { FC, ReactNode } from 'react';
+import { Text } from '../Text/Text';
+import { Tag } from '../Tag/Tag';
+import { TagVariant } from '../Tag/types';
+import { Box } from '../Box/Box';
+import { Colors } from '../../../../theme';
+import cn from 'classnames';
+import * as styles from './Breadcrumbs.css';
+
+export interface BreadCrumbItem {
+  title: string;
+  href?: string;
+  slug?: string[];
+  typename?: string;
+  isTag?: boolean;
+  isCurrentPage?: boolean;
+}
+
+interface BreadcrumbsProps {
+  items: BreadCrumbItem[];
+  label?: string;
+  color?: keyof typeof styles.breadcrumb;
+  tagVariant?: TagVariant;
+  separatorColor?: Colors;
+  renderLink?: (link: ReactNode, item: BreadCrumbItem) => ReactNode;
+}
+
+export const Breadcrumbs: FC<React.PropsWithChildren<BreadcrumbsProps>> = ({
+  items,
+  label = 'breadcrumb',
+  color = 'blue400',
+  tagVariant = 'blue',
+  renderLink = (link) => link,
+}) => {
+  const visibleItems = items.filter((x) => x.title);
+
+  return (
+    <Box
+      aria-label={label}
+      display={'inlineFlex'}
+      flexWrap={'wrap'}
+      alignItems={'center'}
+      className="rs_skip"
+      component="nav"
+    >
+      {visibleItems.map((item, index) => {
+        const isLink: boolean = !!item.href || !!item.slug;
+        const renderCrumb = item.isTag ? (
+          <Tag textLeft={true} disabled={!isLink} variant={tagVariant}>
+            {item.title}
+          </Tag>
+        ) : (
+          <Text
+            as="span"
+            variant={'eyebrow'}
+            color={isLink ? undefined : color}
+          >
+            {item.title}
+          </Text>
+        );
+
+        return (
+          <Box
+            key={`${item.title}-${index}`}
+            display={'inlineFlex'}
+            alignItems={'center'}
+          >
+            {isLink
+              ? renderLink(
+                  <a
+                    href={item?.href}
+                    tabIndex={item.isTag ? -1 : undefined}
+                    {...(item.isCurrentPage && { 'aria-current': 'page' })}
+                    className={cn(
+                      styles.breadcrumb[color],
+                      styles.focusable[color],
+                      { [styles.isTag]: item.isTag },
+                    )}
+                  >
+                    {renderCrumb}
+                  </a>,
+                  item,
+                )
+              : renderCrumb}
+            {visibleItems.length - 1 > index && (
+              <Box
+                borderRadius="full"
+                display={'inlineBlock'}
+                marginY={0}
+                marginX={1}
+                className={cn(styles.bullet, styles.color[color])}
+              ></Box>
+            )}
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
