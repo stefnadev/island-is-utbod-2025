21cad3c11698020381adaed3ac1f3decef7c3ccc
runarvestmann <runar@stefna.is>
2025-05-07 16:26:45 +0000

    Remove unused file

--- apps/gateway-api/src/clients/skattur-api-config.ts
+++
@@ -1,6 +0,0 @@
-import type { CreateClientConfig } from './skattur-api/client.gen';
-
-export const createClientConfig: CreateClientConfig = (config) => ({
-  ...config,
-  baseUrl: 'http://localhost:3001',
-});
