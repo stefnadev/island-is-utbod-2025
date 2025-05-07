2c63dfc2558c8950e2fc1a2e2e3b302611f7c68e
Rúnar Vestmann <runar@stefna.is>
2025-05-07 10:25:17 +0000

    chore(thjodskra-api): Rename project (#24)

--- apps/thjodskra-api-e2e/jest.config.ts
+++ apps/thjodskra-api-e2e/jest.config.ts
@@ -3,14 +3,14 @@ import { readFileSync } from 'fs';
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8'),
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@island2025-alhlidateymi/stafraent-island-api-e2e',
+  displayName: '@island2025-alhlidateymi/thjodskra-api-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
--- apps/thjodskra-api/jest.config.ts
+++ apps/thjodskra-api/jest.config.ts
@@ -3,14 +3,14 @@ import { readFileSync } from 'fs';
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8'),
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@island2025-alhlidateymi/stafraent-island-api',
+  displayName: '@island2025-alhlidateymi/thjodskra-api',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
