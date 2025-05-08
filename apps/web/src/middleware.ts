cb7d7fd1f3eac931f82ab0849b3976d1480472d5
runarvestmann <runar@stefna.is>
2025-05-08 10:40:23 +0000

    Remove login requirement from middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,20 +1,9 @@
-// TODO: Edit this file when we turn in the project
-
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { CookieKeys } from './constants';
 import { verifyJWT } from './utils/jwt';
 
-const [AUTH_USER, AUTH_PASS] = 'island:runarcool'.split(':');
-
 export async function middleware(req: NextRequest) {
-  if (!isAuthenticated(req)) {
-    return new NextResponse('Authentication required', {
-      status: 401,
-      headers: { 'WWW-Authenticate': 'Basic' },
-    });
-  }
-
   if (
     !req.nextUrl.pathname.startsWith('/skattframtol') &&
     !req.nextUrl.pathname.startsWith('/stadfesta-innskraningu') &&
@@ -50,24 +39,6 @@ export async function middleware(req: NextRequest) {
   }
 
   return NextResponse.next();
-}
-
-function isAuthenticated(req: NextRequest) {
-  const authheader =
-    req.headers.get('authorization') || req.headers.get('Authorization');
-
-  if (!authheader) {
-    return false;
-  }
-
-  const auth = Buffer.from(authheader.split(' ')[1], 'base64')
-    .toString()
-    .split(':');
-
-  if (auth[0] == AUTH_USER && auth[1] == AUTH_PASS) {
-    return true;
-  }
-  return false;
 }
 
 export const config = {};
